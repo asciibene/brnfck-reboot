07305af3f672a412bcf628aa74c07c6e69d9bb78
asciibene <ascii-bene@protonmail.ch>
2024-05-08 16:47:19 -0400

    changes

--- brnfck.py
+++ brnfck.py
@@ -5,16 +5,18 @@ class Brnfck():
 
     def __init__(self, tape_size):
 
-        # buffer the brainfuck program manipulates
+        # number of cells
+        self.tape_size=tape_size
+        # buffer (or tape) the brainfuck program manipulates
         self.__tape = bytearray([0] * tape_size)
 
         # pointer to the write/read head on the buffer
         self.__tape_ptr = 0
 
-        # current position in the program
+        # current position in the program (charnumber)
         self.__cmd_ptr = 0
 
-        # current position on the input
+        # current position on the input (hen reading input)
         self.__ipt_ptr = 0
 
         # loop tape pointer stack for loop count pointer positions
@@ -29,23 +31,27 @@ class Brnfck():
 
     # execute a brainfuck programm, optionally with an input
     def run(self, prg, ipt=""):
-
-        # output of the brainfuck program
+        # output of the program
         opt = ""
 
         # evaluate each command from the brainfuck program
-        while (self.__cmd_ptr < len(prg)):
-
-            # get current command from program
+        while (self.__cmd_ptr <= len(prg)):
+            # get current command (chat) from program string
             cmd = prg[self.__cmd_ptr]
-
-            # move the tape pointer one position to the right
+            
+            # move the tape pointer one position to the righht
             if self.__lp_skp_flg and cmd == ">":
-                self.__tape_ptr += 1
+                if self.__tape_ptr < self.tape_size:
+                    self.__tape_ptr += 1
+                elif self.__tape_ptr >= self.tape_size:
+                    self.__tape_ptr=0
 
             # move the tape pointer one position to the left
             elif self.__lp_skp_flg and cmd == "<":
-                self.__tape_ptr -= 1
+                if self.__tape_ptr > 0:
+                    self.__tape_ptr -= 1
+                else:
+                    self.__tape_ptr = self.tape_size          
 
             # increase the current buffer cell by one
             elif self.__lp_skp_flg and cmd == "+":
@@ -59,12 +65,12 @@ class Brnfck():
             elif self.__lp_skp_flg and cmd == ".":
                 opt += chr(self.__tape[self.__tape_ptr])
 
-            # read the current input character in the the current buffer cell
+            # read the current input character and store in the the current buffer cell
             elif self.__lp_skp_flg and cmd == ",":
                 self.__tape[self.__tape_ptr] = ord(ipt[self.__ipt_ptr])
                 self.__ipt_ptr += 1
 
-            # loop over the enclosed commands until the current buffer cell is 0
+        3    # loop over the enclosed commands until the current buffer cell is 0
             elif self.__lp_skp_flg and cmd == "[":
 
                 # store the loop counter for the current loop
@@ -85,28 +91,26 @@ class Brnfck():
                 # check for left iterations of loop
                 if self.__tape[self.__lp_cnt_ptr_stk[-1]] == 0:
 
-                    # remove loop from loop count pointer stack
+                    # remove cell adress from loop count pointer stack
                     self.__lp_cnt_ptr_stk.pop()
-
-                    # remove loop from loop jumpback pointer stack
+                    # remove program "adress" from loop jumpback pointer stack
                     self.__lp_cmd_ptr_stk.pop()
-
+                    
                 else:
-
                     # jump back to the jumpback address of the most inner loop
                     self.__cmd_ptr = self.__lp_cmd_ptr_stk[-1]
 
             # move pointer to next command in brainfuck program
             self.__cmd_ptr += 1
 
-        # print the program's output
+        # print the program's output at the end
         print("%s" % (opt))
         # TODO print execution time
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("================ brnfck v.0.1 ================")
+        print("================ brnfck -- Rebooted v0.12 ================")
         print("USAGE: python brnfck.py \"<program>\" [\"<input>\"]")
-    elif len(sys.argv) == 3:
+    elif len(sys.argv) >= 2:
         bf = Brnfck(30)
         bf.run(sys.argv[1], sys.argv[2] if len(sys.argv) > 2 else "")
